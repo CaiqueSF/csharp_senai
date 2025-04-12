c3e5a0a161358def56ff36004e09b4dc59d171cc
CaiqueSF <caiquedesousaferreira@gmail.com>
2025-04-12 12:59:07 -0300

    feat: finalizado Projeto_Sistema-Bancario

---
+++ AULA_4/Teoria_Aula_4/Teoria_Aula_4/Polimorfismo.cs
@@ -0,0 +1,25 @@
+﻿//Polimorfismo permite que um mesmo método tenha diferentes comportamentos, 
+//dependendo da classe que o implementa.
+
+using System;
+
+public class Veiculo {
+	public virtual void Mover() {
+        Console.WriteLine("O veículo está se movendo.");
+    }
+}
+
+
+public class Carro : Veiculo {
+    public override void Mover() {
+        Console.WriteLine("O carro está andando na estrada.");
+    }
+}
+
+
+public class Aviao : Veiculo {
+    public override void Mover() {
+        Console.WriteLine("O avião está voando.");
+    }
+}
+
